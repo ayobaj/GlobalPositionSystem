8edde26f3d9f618f5fdd63750e0ff271c0a73c36
ayobaj <ayobaj007@gmail.com>
2025-06-18 03:17:59 -0400

    Add: property wrappers

--- GlobalPositionSystem/ForwardGeoScreen.swift
+++ GlobalPositionSystem/ForwardGeoScreen.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct ForwardGeoScreen: View {
+    
+    // PROPERTIES
+    @State private var tfStreet: String = "160 Kendal Avenue"
+    @State private var tfCity: String = "Toronto"
+    @State private var tfCountry: String = "Canada"
+    @State private var result: String = ""
+    
+    @State private var lat: Double = 0.0
+    @State private var lng: Double = 0.0
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
