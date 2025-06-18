8f7ef55fbc907a821d1c3dd44ef4f67e3dbe746b
ayobaj <ayobaj007@gmail.com>
2025-06-18 02:50:05 -0400

    Initial Commit

---
+++ GlobalPositionSystem/GlobalPositionSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalPositionSystemApp.swift
+//  GlobalPositionSystem
+//
+//  Created by Ayomide Bajulaye on 2025-06-18.
+//
+
+import SwiftUI
+
+@main
+struct GlobalPositionSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlobalPositionSystemTests/GlobalPositionSystemTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalPositionSystemTests.swift
+//  GlobalPositionSystemTests
+//
+//  Created by Ayomide Bajulaye on 2025-06-18.
+//
+
+import Testing
+@testable import GlobalPositionSystem
+
+struct GlobalPositionSystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
