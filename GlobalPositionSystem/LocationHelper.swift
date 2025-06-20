bbc116a82a15fa2c60d30adcbcd21fa76f724052
ayobaj <ayobaj007@gmail.com>
2025-06-20 16:08:22 -0400

    feat : Add LocationHelper class with placeholders for forward and reverse geocoding

--- GlobalPositionSystem/LocationHelper.swift
+++ GlobalPositionSystem/LocationHelper.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+// REQUIRED FOR ACCESS THE LOCATION HELPER
+import CoreLocation
+
+
+class LocationHelper: ObservableObject{
+    
+    // AN OBJECT OF CLGEOCODER CLASS TO PERFORM FORWARD AND REVERSE GEOCDING
+    private let geoCoder = CLGeocoder()
+    
+    // PROPERTY TO STORE RESULT OF FWD GEOCODING IN STRING TYPE
+    @Published var fwdGeoLocation: CLLocation = CLLocation(latitude: 0.0, longitude: 0.0)
+    
+    // PROPERTY TO STORE RESULT OF FWD GEOCODING IN STRING TYPE
+    @Published var reverseGeoAddress: String = ""
+    
+    // PERFORM FWD GEOCODING
+    func performFwdGeocoding(){
+        
+    }
+    
+    // PERFORM REVERSE GEOCODING
+    func performReverseGeocoding() {
+        
+    }
+    
+}
