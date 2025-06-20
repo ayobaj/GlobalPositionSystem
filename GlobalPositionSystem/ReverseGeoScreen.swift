e39aba13d8509e03be5fa45d465ee09eea63305c
ayobaj <ayobaj007@gmail.com>
2025-06-20 14:57:45 -0400

    feat: Add reverse geo screen

--- GlobalPositionSystem/ReverseGeoScreen.swift
+++ GlobalPositionSystem/ReverseGeoScreen.swift
@@ -2,14 +2,70 @@
 //  ReverseGeoScreen.swift
 //  GlobalPositionSystem
 //
-//  Created by Ayomide Bajulaye on 2025-06-20.
+//  Created by Ayomide Bajulaye on 2025-06-18.
 //
 
 import SwiftUI
 
 struct ReverseGeoScreen: View {
+    
+    // PROPERTIES
+   
+    @State private var lat: String = "34.778899"
+    @State private var lng: String = "-76.0855446"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack{
+            
+            ContainerRelativeShape()
+                .fill(Color.blue.gradient)
+                .ignoresSafeArea(.all)
+            
+            
+            VStack{
+                
+                Text("Reverse Geo Coding")
+                    .foregroundStyle(.white)
+                    .fontWeight(.bold)
+                    .font(.headline)
+                    .padding()
+                
+                Form{
+                    
+                    // TEXT FIELD - LATITUDE
+                    TextField("Enter Latitude", text: $lat)
+                        .padding(8)
+                    
+                    // TEXT FIELD - LONGITUDE
+                    TextField("Enter Longitude", text: $lng)
+                        .padding(8)
+                    
+                    Text("Result: ")
+                        .foregroundStyle(.red)
+                        .fontWeight(.bold)
+                        .padding()
+                    
+                    
+                    // BUTTON
+                    Button{
+                        
+                        print(#function, "Trying to obtain the lat \(lat) and long \(lng)")
+                    }label: {
+                        Text("Obtain Address")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    
+                }// FORM
+                .scrollContentBackground(.hidden) // FORM CONFORMATION TO THE ZSTACK
+                .background(Color.clear)
+                
+                
+            }// VSTACK
+            
+        }// ZSTACK
+        
     }
 }
 
